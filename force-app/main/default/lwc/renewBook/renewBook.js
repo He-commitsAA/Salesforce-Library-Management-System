45a9593a4d8bbc1184737649f1a2591511b0f78d
Jacob Thompson <jacobthompson9526@gmail.com>
2021-03-14 13:22:22 -0400

    Results update on renewal

--- force-app/main/default/lwc/renewBook/renewBook.js
+++ force-app/main/default/lwc/renewBook/renewBook.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getCheckedOutBooks from '@salesforce/apex/BookController.getCheckedOutBooks';
 import getLoans from '@salesforce/apex/BookController.getLoans';
 import getBookList from '@salesforce/apex/BookController.getBookList';
@@ -7,8 +7,14 @@ import renewLoan from '@salesforce/apex/BookController.renewLoan';
 
 const extension = 14;
 
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate()+1 + days);
+    return result;
+}
+
 export default class RenewBook extends LightningElement {
-    loans;
+    @track loans;
     allLoans;
     copies;
     allBooks;
@@ -16,11 +22,11 @@ export default class RenewBook extends LightningElement {
 
     handleChange(event) {
         let searchQuery = event.target.value;
-        //change shown loans to give best match to query by copy# or title
+        //change shown loans to give best match to query by copy#, title, or borrower
         this.loans = [];
         for (let i in this.allLoans) {
             let l = this.allLoans[i];
-            if (l.Title.includes(searchQuery) || l.Serial.includes(searchQuery)) {
+            if (l.Title.includes(searchQuery) || l.Serial.includes(searchQuery) || l.Member.includes(searchQuery)) {
                 this.loans.push(l);
             }
         }
@@ -29,10 +35,26 @@ export default class RenewBook extends LightningElement {
     renew(event) {
         //renew the book
         let loanId = event.target.name;
-        console.log(loanId);
         renewLoan({ loanId: loanId, days: extension })
             .then(() => {
-                //todo: update the local loan list
+                let extendedDate = undefined;
+                for (let i in this.allLoans) {
+                    let loan = this.allLoans[i];
+                    if (loan.Id === loanId) {
+                        let newDate = addDays(loan.Due__c, extension);
+                        extendedDate = `${newDate.getFullYear()}-${newDate.getMonth()+1}-${newDate.getDate()}`;
+                        this.allLoans[i].Due__c = extendedDate;
+                        break;
+                    }
+                }
+                for (let i in this.loans) {
+                    let loan = this.loans[i];
+                    if (loan.Id === loanId) {
+                        this.loans[i].Due__c = extendedDate;
+                        break;
+                    }
+                }
+                this.loans = [...this.loans];
             }).catch((error) => {
                 console.log(error);
             });
@@ -87,7 +109,7 @@ export default class RenewBook extends LightningElement {
             for (let i in result.data) {
                 let loan = result.data[i];
                 this.allLoans.push({});
-                Object.assign(this.allLoans[i], loan);
+                Object.assign(this.allLoans[i], loan); //copy the entire list to allow for edits
                 let copy = this.copies.get(loan.Book_Copy__c);
                 this.allLoans[i].Serial = copy.Name;
                 this.allLoans[i].Title = this.allBooks.get(copy.Book__c).Name;
