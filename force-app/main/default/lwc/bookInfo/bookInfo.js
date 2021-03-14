542bb130fb60b04c663953e605ccf471771f5d8f
Brent Yurek <brent.yurek@smoothstack.com>
2021-03-14 18:34:15 -0500

    Create bookInfo LWC

---
+++ force-app/main/default/lwc/bookInfo/bookInfo.js
@@ -0,0 +1,24 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS = [
+    'Book__c.Name'
+];
+
+export default class BookInfo extends LightningElement {
+    @api recordId;
+
+    bookInfo;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    getBookInfo(result) {
+        console.log(result);
+        if (result.data) {
+            const bookFields = result.data.fields;
+            var url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookFields.Name.value}`;
+            fetch(url)
+                .then(response => response.json())
+                .then(data => this.bookInfo = data.items[0]);
+        }
+    }
+}
