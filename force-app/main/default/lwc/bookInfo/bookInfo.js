c8c6989fafbb68af7c1cb34c182fce478159b6a2
Brent Yurek <brent.yurek@smoothstack.com>
2021-03-15 18:55:48 -0500

    Added links for ordering online

--- force-app/main/default/lwc/bookInfo/bookInfo.js
+++ force-app/main/default/lwc/bookInfo/bookInfo.js
@@ -25,4 +25,22 @@ export default class BookInfo extends LightningElement {
     get authors() {
         return this.bookInfo.volumeInfo.authors.join(', ');
     }
+
+    get orderLinks() {
+        const isbn = this.bookInfo.volumeInfo.industryIdentifiers[0].identifier;
+        return [
+            {
+                name: 'Barnes & Noble',
+                url: `https://www.barnesandnoble.com/w/_/_?ean=${isbn}`
+            },
+            {
+                name: 'Amazon',
+                url: `http://www.amazon.com/gp/search?index=books&linkCode=qs&keywords=${isbn}`
+            },
+            {
+                name: 'Books-A-Million',
+                url: `http://www.booksamillion.com/product/${isbn}`
+            }
+        ];
+    }
 }
