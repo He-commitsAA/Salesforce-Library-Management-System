882a9a21dbbe4ad42a188ca690e95100abff8712
Brent Yurek <brent.yurek@smoothstack.com>
2021-04-14 12:57:02 -0500

    Use more fields to find book info

--- force-app/main/default/lwc/bookInfo/bookInfo.js
+++ force-app/main/default/lwc/bookInfo/bookInfo.js
@@ -2,7 +2,9 @@ import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 
 const FIELDS = [
-    'Book__c.Name'
+    'Book__c.Name',
+    'Book__c.Author__c',
+    'Book__c.ISBN__c'
 ];
 
 export default class BookInfo extends LightningElement {
@@ -15,7 +17,15 @@ export default class BookInfo extends LightningElement {
         console.log(result);
         if (result.data) {
             const bookFields = result.data.fields;
-            var url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookFields.Name.value}`;
+            var url = `https://www.googleapis.com/books/v1/volumes?q=`;
+            if (bookFields.ISBN__c.value) {
+                url += `isbn:${bookFields.ISBN__c.value}`;
+            } else if (bookFields.Name.value) {
+                url += `intitle:${bookFields.Name.value}`;
+                if (bookFields.Author__c.value) {
+                    url += `+inauthor:${bookFields.Author__c.value}`;
+                }
+            }
             fetch(url)
                 .then(response => response.json())
                 .then(data => this.bookInfo = data.items[0]);
